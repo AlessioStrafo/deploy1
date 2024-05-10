edf1802d3d45138970d4055f30d8a5d1fedf48c1
AlessioStrafo <straforini.alessio@gmail.com>
2024-05-10 12:56:21 +0200

    change version into pom

--- src/main/java/co/develhope/deploy1/NameController.java
+++ src/main/java/co/develhope/deploy1/NameController.java
@@ -1,5 +1,9 @@
 package co.develhope.deploy1;
-
+//write a Spring Boot application with the necessary dependencies that:
+//has a basic controller that returns a property devName that has the value of your name
+//runs on port 7070
+//the version is 0.0.3
+//create the .jar and launch it using the command line
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
